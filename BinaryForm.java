95dc1da48832cedfaeb62d3d65fb0b81ebe10fb9
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-25 19:15:44 +0530

    Implementation done

--- BinaryForm.java
+++ BinaryForm.java
@@ -1,19 +1,28 @@
 import java.io.*;
 /**
-*A simple program in JAVA to convert a given number to its equivalent binary form using <b>BitWise operatiions</b>
+*A simple program in JAVA to convert a given number to its equivalent binary form using only <b>BitWise operations</b>
 *
 *@author: Devesh Shetty
 */
 public class BinaryForm{
     
     public static void main(String[] args) throws Exception{
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
-        PrintWriter out = new PrintWriter(System.out);
+        PrintStream out = System.out;
+        
         out.print("Enter a number: ");
         int n = Integer.parseInt(br.readLine());
         
         out.println("Binary form of "+n+": ");
         
+        //int is of 4 bytes, hence 32 bits
+        for(int i = 31; i>=0; i--){
+            //make use of the unsigned right shift operator
+            // and & it with 1 to get the bit at the 'i'th position
+            out.print( ( n >>> i ) & 1 );
+        }
+        out.println();
     }
     
 }

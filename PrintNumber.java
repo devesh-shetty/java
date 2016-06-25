0cc598823054eab801a130ade0e4297cd01c3601
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-25 19:30:40 +0530

    A program to print  a number in words

---
+++ PrintNumber.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+/**
+*A program to print a given number in words
+*
+*@author: Devesh Shetty
+*/
+public class PrintNumber{
+
+    public static void main(String[] args){
+    
+    }
+    
+    /**
+    *This method takes a number and prints it's word equivalent
+    *@param n the number
+    */
+    private static void printNumberInWordFormat(int n){
+    
+    }
+    
+}

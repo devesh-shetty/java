bcceeca026ffd57e539292224fa307a5b5aca2f0
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-26 02:56:06 +0530

    Exploring String in JAVA

---
+++ StringExplorer.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+/**
+*This program aims to explore the String and its different methods in JAVA
+*@author: Devesh Shetty
+*/
+public class StringExplorer{
+    
+    public static void main(String[] args){
+        //In JAVA, String is an object
+        //String object can be stored in the heap or in the string pool
+        
+        
+        String x = "Hello";
+        
+        
+    }
+    
+}

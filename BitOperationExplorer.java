bd4e0f0dd3db289e25bb50fb357692393382660b
Devesh Shetty <devesh.shetty007@gmail.com>
2016-07-08 17:09:56 +0530

    Left shift and right shift oeprations

--- BitOperationExplorer.java
+++ BitOperationExplorer.java
@@ -1,5 +1,6 @@
+import java.io.PrintStream;
 /**
-*Aim to explore and implement efficient operations that can be performed with bitwise manipulation
+*Aim to explore and implement efficient operations that can be performed with bit manipulation
 *
 *@author: Devesh Shetty
 */
@@ -7,7 +8,14 @@ class Main{
     
     public static void main(String[] args){
         
+        PrintStream out = System.out;
+        //shifting 1 bit to left multiplies the number by 2
+        out.println("1 << 1 is basically shifting 1 bit to left(multiplying by 2), to get: " + ( 1 << 1));
         
+        int n = 10;
+        out.println(n+" << 1 = "+ (n << 1));
+        out.println(n+" >> 1 = "+ (n >> 1));
+        out.println(n+" << 2 = "+ (n << 2));
         
     }
     

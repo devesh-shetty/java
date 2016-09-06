5fc7b198a7263d3166c21bab65c57ea5c6c61b46
Devesh Shetty <devesh.shetty007@gmail.com>
2016-09-07 02:15:35 +0530

    Getting all the fields of a class using Reflection API

--- com/test/reflection/BookReflectionExplorer.java
+++ com/test/reflection/BookReflectionExplorer.java
@@ -2,6 +2,8 @@ package com.test.reflection;
 
 import java.util.Arrays;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
 *
@@ -32,6 +34,45 @@ public class BookReflectionExplorer{
             System.out.println("Parameter count: "+constructor.getParameterCount());
             System.out.println("Parameters: "+Arrays.toString(constructor.getParameterTypes()));
         }
+        
+        //get all the fields (public as well as private)
+        Field[] fields = clsBook.getDeclaredFields();
+        System.out.println("The class has "+fields.length+"  fields(public as well as private)" );
+        for(Field field: fields){
+            System.out.println("Field Name: "+field.getName());
+            System.out.println("Type: "+field.getType());
+        }
+        
+        //get all the public fields 
+        fields = clsBook.getFields();
+        System.out.println("The class has "+fields.length+"  public fields" );
+        for(Field field: fields){
+            System.out.println("Field Name: "+field.getName());
+            System.out.println("Type: "+field.getType());
+        }
+        
+        //gets all the methods (public as well as private) excludes inherited methods
+        Method[] methods = clsBook.getDeclaredMethods();
+        System.out.println("The class has "+methods.length+"  methods(public as well as private)" );
+        for(Method method: methods){
+            System.out.println();
+            System.out.println("Method Name: "+method.getName());
+            System.out.println("Parameter count: "+method.getParameterCount());
+            System.out.println("Parameters: "+Arrays.toString(method.getParameterTypes()));
+            System.out.println("Return Type: "+method.getReturnType());
+        }
+        
+        //gets all the public methods as well as those inherited
+        methods = clsBook.getMethods();
+        System.out.println("\nThe class has "+methods.length+"  public methods" );
+        for(Method method: methods){
+            System.out.println();
+            System.out.println("Method Name: "+method.getName());
+            System.out.println("Parameter count: "+method.getParameterCount());
+            System.out.println("Parameters: "+Arrays.toString(method.getParameterTypes()));
+            System.out.println("Return Type: "+method.getReturnType());
+        }
+        
         
     }
     

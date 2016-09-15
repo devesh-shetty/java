d7d42bb5fb40863bad9508f949fe43f32a2b2e6d
Devesh Shetty <devesh.shetty007@gmail.com>
2016-09-15 19:43:34 +0530

    Creating Runnables through lambda

--- com/test/java8/NewFeatures.java
+++ com/test/java8/NewFeatures.java
@@ -1,3 +1,4 @@
+package com.test.java8;
 /**
 *Exploring new JAVA8 features
 *
@@ -8,11 +9,23 @@ public class NewFeatures{
     public static void main(String[] args){
         
         //Lambda expression
+        //We can use Lambda expression to replace anonymous inner classes
+        //Constraint: The interface must have only 1 abstract method
+        // -> marks the body of the lambda expression
+        //Thread constructor takes a Runnable
+        //which has a single abstract method public void run()
+        //Our lambda expression will be based on this single abstract method
+        //run() takes no arguments so our lambda will take no argument
         Thread one = new Thread(() -> {
             System.out.println("Thread One");
         });
         
+        //When we have a single statement in the body of the lambda expression, 
+        //we can skip the '{}' braces and directly write that 1 statement
+        Thread two = new Thread( () -> System.out.println("Thread Two"));
+        
         one.start();
+        two.start();
         
     }
 }

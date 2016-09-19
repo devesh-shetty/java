a3087ece65accc8d62397b73b2af876f9e29f246
Devesh Shetty <devesh.shetty007@gmail.com>
2016-09-19 23:19:45 +0530

    A utility class to help us with operations on list using functional interfaces

---
+++ com/test/java8/ListUtil.java
@@ -0,0 +1,12 @@
+package com.test.java8;
+
+/**
+*A utility class to help us with operations on list using functional interfaces
+*
+*@author: Devesh Shetty
+*/
+public class ListUtil{
+    
+    
+    
+}

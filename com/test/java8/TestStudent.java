c9ce09205d8094e63c486d8e8d4101efaa199418
Devesh Shetty <devesh.shetty007@gmail.com>
2016-10-05 01:00:08 +0530

    Made use of a Stream Supplier

--- com/test/java8/TestStudent.java
+++ com/test/java8/TestStudent.java
@@ -9,7 +9,8 @@ import java.util.stream.Collectors;
 import java.util.HashMap;
 import java.util.List;
 import java.util.OptionalDouble;
-
+import java.util.stream.Stream;
+import java.util.function.Supplier;
 /**
 *@author: Devesh Shetty
 */
@@ -37,23 +38,23 @@ public class TestStudent{
         
         studentList.forEach(System.out :: println);
         
-        //A typical way of storing data in a map
-//        HashMap<String, String> map = new HashMap<String, String>();
-//        studentList.forEach( student -> {
-//           String key = student.getGender().value();
-//            if(!map.containsKey(key)){
-//            
-//               map.put(key, student.getName());
-//           }else{
-//                //map already has a value with that key
-                    // so handle the collision by separating the values with "|"
-//                String name = map.get(key);
-//                name+= "|"+student.getName();
-//                map.put(key, name);
-//            } 
-//        });
-//        System.out.println(map);
-        
+     //   A typical way of storing data in a map
+    /*    HashMap<String, String> map = new HashMap<String, String>();
+        studentList.forEach( student -> {
+           String key = student.getGender().value();
+            if(!map.containsKey(key)){
+            
+               map.put(key, student.getName());
+           }else{
+                //map already has a value with that key
+                     so handle the collision by separating the values with "|"
+                String name = map.get(key);
+                name+= "|"+student.getName();
+                map.put(key, name);
+            } 
+        });
+        System.out.println(map);
+      */  
         Map<String, String> map = studentList.stream()
                                             .collect(Collectors.toMap( student -> student.getGender().value(),//the key
                                                                       student -> student.getName(),//the value
@@ -63,25 +64,42 @@ public class TestStudent{
         
         map.forEach((key, value) -> System.out.println(key+": "+value));
         
+        //Now, instead of creating multiple streams of male and female, why not reuse the old one?
+        //once after performing a terminal operation on the stream,
+        //If we try to reuse the same stream after performing a terminal operation on it, we get 
+        //java.lang.IllegalStateException:"stream has already been operated upon or closed"
+        //so instead of reusing the existing stream, we make use of a Stream Supplier
+        //that provides a new Stream for all our future purposes
+        
+        /*Stream<Student> maleStream = studentList.stream()
+                                                .filter(Student :: isMale);
+        
+        Stream<Student> femaleStream = studentList.stream()
+                                                    .filter(Student :: isFemale);
+        */
+        Supplier< Stream<Student> > maleStreamSupplier = new Supplier< Stream<Student> >(){
+                                                                    //anonymous inner class syntax
+                                                                    public Stream<Student> get(){
+                                                                        return studentList.stream().filter(Student :: isMale);
+                                                                    }
+                                                                };
+        //using lambda expressions
+        Supplier< Stream<Student> > femaleStreamSupplier = () -> studentList.stream().filter(Student :: isFemale);
+        
+        
         //Name of all male candidates
-        studentList.stream()
-            .filter( Student :: isMale )
-            .forEach( student -> System.out.print(student.getName()+" "));
+        maleStreamSupplier.get().forEach( student -> System.out.print(student.getName()+" "));//terminal operation;
         System.out.println();
         
         //name of all female candidates with age > 20
-        List<Student> femalesAgeGreater20 = studentList.stream()
-                                                    .filter( Student :: isFemale )
-                                                    .filter( Student :: isAgeGreaterThan20 )
+        List<Student> femalesAgeGreater20 = femaleStreamSupplier.get().filter( Student :: isAgeGreaterThan20 )
                                                     .collect( Collectors.toList() );
         femalesAgeGreater20.forEach(System.out :: println);
         System.out.println();
         
         //Average age of all males in the class
         //OptionalDouble uses the NULL object pattern
-        OptionalDouble optionalAverage = studentList.stream()
-                                                    .filter( Student :: isMale )
-                                                    .mapToInt( Student :: getAge)
+        OptionalDouble optionalAverage = maleStreamSupplier.get().mapToInt( Student :: getAge )
                                                     .average();
         
         if(optionalAverage.isPresent()){
@@ -91,9 +109,7 @@ public class TestStudent{
         }
         
         //get the maximum score secured by a female
-        OptionalDouble optionalMax = studentList.stream()
-                                            .filter( Student :: isFemale )
-                                            .mapToDouble( Student :: getMarks)
+        OptionalDouble optionalMax = femaleStreamSupplier.get().mapToDouble( Student :: getMarks)
                                             .max();
         
         if(optionalMax.isPresent()){
